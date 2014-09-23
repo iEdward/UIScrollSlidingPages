9274441f367a5c6f7b0d2f588717c7463e388876
Anthony Powles <github@idreamz.net>
2014-09-23 15:27:40 -0700

    Fixed some warnings

--- UIScrollViewSlidingPages/Source/TTScrollSlidingPagesController.m
+++ UIScrollViewSlidingPages/Source/TTScrollSlidingPagesController.m
@@ -215,6 +215,8 @@
 }
 
 -(void)viewDidAppear:(BOOL)animated{
+    [self viewDidAppear:animated];
+
     if (!viewDidAppearHasBeenCalled){
         viewDidAppearHasBeenCalled = YES;
         [self reloadPages];
@@ -708,7 +710,7 @@
 -(void)pageControlChangedPage:(id)sender
 {
     //if not already on the page and the page is within the bounds of the pages we have, scroll to the page!
-    int page = pageControl.currentPage;
+    int page = (int)pageControl.currentPage;
     if ([self getCurrentDisplayedPage] != page && page < [bottomScrollView.subviews count]){
         [self scrollToPage:page animated:YES];
     }
